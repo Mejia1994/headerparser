709db77998fb902edb4b76e947524c445babd54f
Mejia1994 <jrafa1994@gmail.com>
2021-06-02 22:18:49 -0600

    add

--- index.js
+++ index.js
@@ -3,14 +3,30 @@ const app = express();
 const cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}))
 
+const mustache = require("mustache-express")
+app.engine('html', mustache())
+app.set('view engine', 'html')
+app.set('views', __dirname + "/views");
+
 const RequestIp = require('@supercharge/request-ip')
 
+app.use(express.static('public'));
+
+app.get("/", function (req, res) {
+    const data = {
+        ipaddress: RequestIp.getClientIp(req),
+        language: req.header('accept-language'),
+        software: req.header('user-agent')
+    };
+
+    res.render('index', data);
+});
 
 app.get('/api/whoami', function (req, res) {
 
     res.json({
         ipaddress: RequestIp.getClientIp(req),
-        language : req.header('accept-language'),
+        language: req.header('accept-language'),
         software: req.header('user-agent')
     });
 });
